b51adf32f2cddc578d794d9c7d0091ab842bdcb8
Devep01 <devep01.strx@gmail.com>
2023-09-02 10:57:55 -0500

    [ADD] Manifest

---
+++ new_module/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Trade Manager",
+    'summary': "Trade Manager",
+    'author': "Cristina Barreno",
+    'category': 'Stock',
+    'depends': ['base'],
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3'
+   }
